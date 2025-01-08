b4185ab13b84f70f0a73c9ea6f7fe5133ba296ef
Ralph R <ralphrobinson4299@gmail.com>
2025-01-08 09:55:37 -0800

    Starting research on graphql
    might have to move graphql component if it needs to be in a new spot

---
+++ client/src/app/graphql/graphql.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graphql',
+  standalone: true,
+  imports: [],
+  templateUrl: './graphql.component.html',
+  styles: ``
+})
+export class GraphqlComponent {
+
+}
